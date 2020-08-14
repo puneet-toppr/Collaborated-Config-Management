d6b73a589be67787581e7734d826aa9bc6f32d5f
kuldeep-toppr <kuldeep.s@toppr.com>
2020-08-14 18:39:02 +0530

    get all and post function with base urls

--- config_management/config_management/urls.py
+++ config_management/config_management/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('domain/', include('domains.urls')),
+    path('feature/', include('features.urls')),
 ]
