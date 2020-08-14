9b1956d7a9e66f8bf93989827f86d7674e0a891c
cpuneet98 <cpuneet98@gmail.com>
2020-08-14 23:45:21 +0530

    Hopefully all requested changes in last PR are resolved :)

--- config_management/domains/views.py
+++ config_management/domains/views.py
@@ -6,64 +6,63 @@ import json
 # Create your views here.
 class DomainView(View):
 
-    def get(self, request, d_id):
+    def get(self, request, d_id): #get a domain name using its id
 
-        if d_id == 'all':
-            all_domains = Domain.objects.all()
-
-            domain_info = []
-            domain_names = []
-            domain_ids = []
-
-            for i in all_domains:
-                domain_info.append((i.get_info()['id'], i.get_info()['name']))
-                domain_names.append(i.get_info()['name'])
-                domain_ids.append(i.get_info()['id'])
-
-            args = {'domain_info':domain_info, 'domain_names':domain_names, 'domain_ids':domain_ids}
-
-            return JsonResponse(args)
-
+        if Domain.objects.filter(id=d_id).exists(): #raising error if domain with provided id doesn't exist
+            domain_object = Domain.objects.get(id=d_id) 
         else:
-
-            try:
-                domain_object = Domain.objects.get(id=d_id)
-            except:
-                return JsonResponse({'error_message':'The requested domain does not exist'})
+            return JsonResponse({'error_message':'The requested domain does not exist'})
                 
-            domain_name = domain_object.get_info()['name']
-            domain_id = domain_object.get_info()['id']
+        args = {'domain_info':domain_object.get_info()} 
 
-            args = {'domain_name':domain_name, 'domain_id':domain_id}
-
-            return JsonResponse(args)
-
-
-    def post(self, request):
-            
-        json_data = json.loads(str(request.body, encoding='utf-8'))
-        
-        domain_name = (json_data['name'])
-        Domain.objects.create(name=domain_name)
-        domain_id = Domain.objects.get(name=domain_name).get_info()['id']
-
-        args = {'domain_name':domain_name, 'domain_id':domain_id}
-        return JsonResponse(args)
+        return JsonResponse(args) #returning domain's name and id
 
 
     def delete(self, request, d_id):
         
-        try:
-            domain_object = Domain.objects.get(id=d_id)
-
-            name = domain_object.name
-
-            domain_name = domain_object.get_info()['name']
-            domain_id = domain_object.get_info()['id']
-        except:
-            return JsonResponse({'error_message': 'The requested domain does not exist'})
+        if Domain.objects.filter(id=d_id).exists(): #raising error if domain with provided id doesn't exist
+            domain_object = Domain.objects.get(id=d_id) #fetching the object to be deleted if it exists
+            domain_info = domain_object.get_info() #after delete operation, domain_id becomes null
+        else:
+            return JsonResponse({'error_message': 'The requested domain does not exist'}) 
 
         domain_object.delete()
-        args = {'domain_name':domain_name, 'domain_id':domain_id}
+        args = {'domain_info':domain_info}
 
-        return JsonResponse(args)
+        return JsonResponse(args) #returning deleted domain's name and id
+
+class DomainGetPost(View):
+
+    def get(self, request): #fetch list of all the domains
+
+        domains = Domain.objects.all() #fetch all domain objects
+        domains_info = []
+
+        for domain in domains:
+            domains_info.append(domain.get_info()) #storing name and id of all domains in a list
+
+        args = {'domains_info':domains_info}
+        return JsonResponse(args) #returning list containing domain's ids and names
+
+
+    def post(self, request):
+        
+        try: #raising error if body cannot be parsed 
+            json_data = json.loads(str(request.body, encoding='utf-8'))
+        except:
+            return JsonResponse({'error_message':'posted body cannot be parsed, post in json format'})
+
+        try: #raising error if domain name is not under 'name' key
+            domain_name = (json_data['name'])
+        except:
+            return JsonResponse({'error_message':'Kindly post the domain name under \'name\' key'})
+
+        #checking if domain with provided name already exists
+        if Domain.objects.filter(name=(domain_name.lower())).exists(): 
+            return JsonResponse({'error_message':('domain ' + str(domain_name.lower()) + ' already exists')})
+
+        domain_object = Domain.objects.create(name=(domain_name.lower())) 
+        #creating a new domain object if it doesn't exist
+
+        args = {'domain_info':domain_object.get_info()}
+        return JsonResponse(args) #returning added domain's name and id
