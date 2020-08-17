c2c2af42309cbb660ceaf0648e26b4af3f934826
puneet-toppr <69340457+puneet-toppr@users.noreply.github.com>
2020-08-17 22:02:15 +0530

    changes made to domain views, extended to support domain-feature-mapping model

--- config_management/domains/models.py
+++ config_management/domains/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+# from domain_feature_mapping.models import DomainFeatureMapping
+
 
 # Create your models here.
 
@@ -15,8 +17,8 @@ class Domain(models.Model):
     name = models.CharField(max_length=200, unique=True, validators=[validate_case_sentitivity])
 
     def __str__(self):
-        return self.name
+        return str(self.id)
 
     def get_info(self):
-    	return ({'name':self.name, 'id':self.id})
 
+    	return ({'name':self.name, 'id':(self.id)})
